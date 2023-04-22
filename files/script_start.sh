d93bf5b266246c3385846493946a6f585a056f11
Tono Villarejo Reig <tonovillarejo@gmail.com>
2023-04-22 15:42:57 +0200

    fix script

--- files/script_start.sh
+++ files/script_start.sh
@@ -6,12 +6,12 @@
 #    By: tvillare <tvillare@student.42madrid.com    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/04/22 00:21:09 by tvillare          #+#    #+#              #
-#    Updated: 2023/04/22 15:14:21 by tvillare         ###   ########.fr        #
+#    Updated: 2023/04/22 15:39:40 by tvillare         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 !#/bin/sh
-service ssh reload
+service ssh restart
 service nginx start
 service tor start
 cp /var/lib/tor/my_website/hostname ~/domain_onion.txt
